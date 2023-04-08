0c29b94879b85a904533a895c80659c4e4056e20
harsimran <harsimran.iit@gmail.com>
2023-04-08 21:29:28 +0530

    Implementing Stock Market News

---
+++ app/src/main/java/com/workshop/onebytenpredictors/Adapter.java
@@ -0,0 +1,76 @@
+package com.workshop.onebytenpredictors;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Context context;
+    ArrayList<ModelClass> modelClassArrayList;
+
+    public Adapter(Context context, ArrayList<ModelClass> modelClassArrayList) {
+        this.context = context;
+        this.modelClassArrayList = modelClassArrayList;
+    }
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.layout_news,null, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(context, WebViewActivity.class);
+                intent.putExtra("url", modelClassArrayList.get(position).getUrl());
+                context.startActivity(intent);
+            }
+        });
+
+        holder.mTime.setText(modelClassArrayList.get(position).getPublishedAt());
+        holder.mAuthor.setText(modelClassArrayList.get(position).getAuthor());
+        holder.mHeading.setText(modelClassArrayList.get(position).getTitle());
+        Glide.with(context).load(modelClassArrayList.get(position).getUrlToImage()).into(holder.imageView);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelClassArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView mHeading, mAuthor, mTime;
+        CardView cardView;
+        ImageView imageView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mHeading=itemView.findViewById(R.id.textNews);
+            mAuthor=itemView.findViewById(R.id.textAuthor);
+            mTime=itemView.findViewById(R.id.textTime);
+            imageView=itemView.findViewById(R.id.imgNews);
+            cardView=itemView.findViewById(R.id.cardView);
+        }
+    }
+}
--- app/src/main/java/com/workshop/onebytenpredictors/MainActivity.kt
+++ app/src/main/java/com/workshop/onebytenpredictors/MainActivity.kt
@@ -1,26 +1,78 @@
 package com.workshop.onebytenpredictors
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.workshop.onebytenpredictors.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class MainActivity : AppCompatActivity() {
+
+    val api="7cb2b436829844d884370c59671ee9ca"
+    val country="in"
+    lateinit var modelClassArrayList:ArrayList<ModelClass>
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var adapter:Adapter
+
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.layoutNew.visibility=View.GONE
+        binding.buttonPredict.isEnabled=false
 
 
-        binding.buttonPredict.setOnClickListener{
-            binding.layoutDefault.visibility= View.GONE
-            binding.layoutNew.visibility=View.VISIBLE
+        binding.etText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!binding.etText.text.isNullOrEmpty()) {
+                    binding.buttonPredict.isEnabled=true
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+        binding.buttonPredict.setOnClickListener {
+            binding.layoutDefault.visibility = View.GONE
+
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = Adapter(this@MainActivity, modelClassArrayList)
+//            binding.recyclerDefault.adapter = adapter
+//            binding.recyclerDefault.layoutManager = layoutManager
+
+//            findNews()
+
+            binding.textV.setText(binding.etText.text)
+            binding.layoutNew.visibility = View.VISIBLE
         }
     }
+
+//    private fun findNews(){
+//        ApiUtilities.getApiInterface().getNews(country,100,api).enqueue(new Callback<NewsArrayClass>() {
+//            @Override
+//            public fun onResponse(Call<NewsArrayClass> call, Response<NewsArrayClass> response){
+//
+//            }
+//        });
+//    }
 
 
 }
